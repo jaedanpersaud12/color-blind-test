22fa0a5f505454bb3eb75dcc1920152e9aec3849
jaedanpersaud12 <jaedanpersaud12@gmail.com>
2022-01-29 11:59:14 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const container = document.querySelector('.row')
+const colorToGuess = document.querySelector('#color')
+const correctDisplay = document.querySelector('#correct')
+const wrongDisplay = document.querySelector('#wrong')
+const headerDisplay = document.querySelector('#header')
+const resetButton = document.querySelector('#reset-button')
+
+const colorArray = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+let correctCount = 0
+let wrongCount = 0
+correctDisplay.textContent = correctCount
+wrongDisplay.textContent = wrongCount
+
+currentColor = getRandomColor()
+colorToGuess.textContent = currentColor
+
+function getRandomColor() {
+    let random = Math.floor(Math.random() * colorArray.length)
+    return colorArray[random]
+}
+
+container.addEventListener('click', e => {
+    if (e.target.id == currentColor) {
+        headerDisplay.textContent = "GOOD JOB"
+        headerDisplay.classList.remove("text-danger")
+        headerDisplay.classList.add("text-success")
+        currentColor = getRandomColor()
+        colorToGuess.textContent = currentColor
+        correctCount++
+        correctDisplay.textContent = correctCount
+
+    } else {
+        headerDisplay.textContent = "TRY AGAIN"
+        headerDisplay.classList.remove("text-success")
+
+        headerDisplay.classList.add("text-danger")
+        wrongCount++
+        wrongDisplay.textContent = wrongCount
+
+    }
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    correctCount = 0
+    wrongCount = 0
+    correctDisplay.textContent = 0
+    wrongDisplay.textContent = 0
+    headerDisplay.classList.remove("text-success", "text-danger")
+    headerDisplay.textContent = "COLOR BLIND TEST"
+    currentColor = getRandomColor()
+    colorToGuess.textContent = currentColor
+}
